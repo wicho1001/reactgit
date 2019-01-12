fc5a56cb1eacf62c2bc87592fa5fa34a99e6943f
Jose Guerrero <wichoguerrero@yahoo.com>
2019-01-12 14:48:28 -0600

    Updates

---
+++ precache-manifest.1dbae227790b5f03e6be8913ccdb23b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactgit/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7851571893eaeb85c55a",
+    "url": "/reactgit/static/js/runtime~main.78515718.js"
+  },
+  {
+    "revision": "5e4eb5de6f51951018d5",
+    "url": "/reactgit/static/js/main.5e4eb5de.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/reactgit/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "5e4eb5de6f51951018d5",
+    "url": "/reactgit/static/css/main.ea48dce2.chunk.css"
+  },
+  {
+    "revision": "46e60ec2454a8d10ae5700b5e5690c4a",
+    "url": "/reactgit/index.html"
+  }
+];
